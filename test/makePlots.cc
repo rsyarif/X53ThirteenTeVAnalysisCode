8a1c0f8a81183dbadafccde1b0eeb340d288be86
Clint <clint.a.richardson@gmail.com>
2015-06-09 05:38:24 -0500

    working version, need to add legend and customizing axis ranges/binning

--- test/makePlots.cc
+++ test/makePlots.cc
@@ -11,117 +11,111 @@
 #include "TCanvas.h"
 
 
-std::map<std::string, std::map<float,float> > makeBkgSampleVector();
-std::map<std::string, float> makeBkgLumiWeights( std::map<std::string, std::map<float,float> > vBkg, float lumi);
-std::vector<Sample*> getSampleVec(std::map<std::string, float> vBkg, std::string cut);
+
+std::vector<Sample*> getSampleVec(std::string cut,float lumi);
 
 void DrawAndSave(std::string s, std::vector<Sample*> vBkg);
-TCanvas DrawCanvas(std::string variable, std::vector<Sample*> vBkg);
+//TCanvas* DrawCanvas(std::string variable, std::vector<Sample*> vBkg);
   
 void makePlots(){
   
-  //make list of backgrounds and x-sec, and number of events actually used
-  std::map<std::string,std::map<float,float> > vBkgMap = makeBkgSampleVector();
  
-  //make weights based on lumi desired:
+  //desired lumi:
   float lumi = 5.0; //fb^-1
-  std::map<std::string,float> vBkgLumiWeights = makeBkgLumiWeights(vBkgMap,lumi);
 
-  std::vector<Sample*> vBkgSamples = getSampleVec(vBkgLumiWeights, "ssdl");
+
+  std::vector<Sample*> vBkgSamples = getSampleVec("ssdl", lumi);
 
   DrawAndSave("Lep1Pt",vBkgSamples);
   DrawAndSave("Lep2Pt",vBkgSamples);
-  DrawAndSave("HT",vBkgSamples);
+  DrawAndSave("AK4HT",vBkgSamples);
 
 
 }
 
+std::vector<Sample*> getSampleVec(std::string cut, float lumi){
 
-std::map<std::string,std::map<float, float> > makeBkgSampleVector(){
-  
-  std::map<std::string, std::map<float, float> > vec;
+   //setup info for list of samples, xsec and events run
+  std::vector<std::string> vBkgNames;
+  vBkgNames.push_back("TTbar");vBkgNames.push_back("TTZ");vBkgNames.push_back("WZ");vBkgNames.push_back("WJets");
+  //make vector of x-sec (in pb)
+  std::vector<float> vXsec;
+  vXsec.push_back(    491.2);  vXsec.push_back(  2.232); vXsec.push_back(    1.634); vXsec.push_back(   3*50100);
+  //make vector of actual number of events run
+  std::vector<int> vNEvts;
+  vNEvts.push_back( 2206600); vNEvts.push_back(249275);  vNEvts.push_back( 237484); vNEvts.push_back(   3828404);
 
-  vec["TTbar"][491.2]=2206600;
-  vec["TTZ"][2.232]=249275;
-  vec["WZ"][1.634]=237484;
-  vec["WJets"][50100]=3828404;
-
-  return vec;
-
-  //number of events actually processed:
-  //ttbar: 2206600
-  //ttz: 249275
-  //WZ: 237484
-  //WJets: 3828404
-  //ttW: 246521
-
-}
-
-std::map<std::string, float> makeBkgLumiWeights( std::map<std::string, std::map<float,float> > bkgmap, float lumi){
-  std::map<std::string, float> map;
-
-  for( std::map<std::string, std::map<float,float> >::iterator iter = bkgmap.begin(); iter!=bkgmap.end(); iter++){
-    std::cout<<iter->first<<std::endl;
-    //there is only ever one entry for the stored map pointed to by the string, so just get the first element of it via begin() function
-    float weight = (lumi * ((iter->second).begin())->first)/ ( ((iter->second).begin())->second);
-    map.insert(std::pair<std::string,float>(iter->first,weight));
+  //now make vector to hold weights;
+  std::vector<float> vWeights;
+  for(unsigned int ui=0; ui<vXsec.size(); ui++){
+    vWeights.push_back( lumi * ( vXsec.at(ui) / vNEvts.at(ui) ) );
   }
-  return map;
-}
 
- std::vector<Sample*> getSampleVec(std::map<std::string, float > vBkg, std::string cut){
+
+  //for(unsigned int ui=0; ui<vBkgNames.size(); ui++){
+  //std::cout<<"Number: "<<ui<<" Name: "<<vBkgNames.at(ui)<<" xsec: "<<vXsec.at(ui)<<" weight: "<<vWeights.at(ui)<<std::endl;
+  //}
 
   std::vector<Sample*> vSample;
+  TFile* ttfile = new TFile("TTZ.root");
+  Sample* ttSample = new Sample(vBkgNames.at(0),ttfile, vWeights.at(0),vXsec.at(0),cut,kRed+2);
+  vSample.push_back(ttSample);
+  TFile* ttZfile = new TFile("TTZ.root");
+  Sample* ttZSample = new Sample(vBkgNames.at(1),ttZfile, vWeights.at(1),vXsec.at(1),cut,kYellow-2);
+  vSample.push_back(ttZSample);
+  TFile* wzfile = new TFile("WZ.root");
+  Sample* wzSample = new Sample(vBkgNames.at(2),wzfile, vWeights.at(2),vXsec.at(2),cut,kBlue-3);
+  vSample.push_back(wzSample);
+  TFile* wjfile = new TFile("WJets.root");
+  Sample* wjSample = new Sample(vBkgNames.at(3),wjfile, vWeights.at(3),vXsec.at(3),cut,kGreen+2);
+  vSample.push_back(wjSample);
 
-  //int for color iteration
-  int i =0;
-  for(std::map<std::string,float>::iterator iter=vBkg.begin(); iter!=vBkg.end(); iter++){
-    std::string filename = iter->first+".root";
-    TFile* f = new TFile(filename.c_str());
-    //TTree* t = (TTree*)f->Get("tEvts_ssdl");
-    int color = i*4+300;
-    Sample* s = new Sample(iter->first,f,iter->second,color,cut);
 
-    vSample.push_back(s);
-    i++;
-  }
 
   return vSample;
 
 }
 
 
+/*void DrawAndSave(std::string variable, std::vector<Sample*> vBkg){
+
+  TCanvas * c1 = DrawCanvas(variable, vBkg);  
+
+  }*/
+
 void DrawAndSave(std::string variable, std::vector<Sample*> vBkg){
 
-  TCanvas c1 = DrawCanvas(variable, vBkg);
+  TCanvas* c1 = new TCanvas("c1","c1");
 
-  std::string pdfname = variable+"_"+(vBkg[0]->cutname)+".pdf";
+  c1->SetLogy();
 
-  c1.Print(pdfname.c_str());
-  
+  THStack* tStack = new THStack("tStack","");
 
-}
-
-TCanvas DrawCanvas(std::string variable, std::vector<Sample*> vBkg){
-
-  TCanvas c;
-
-  THStack tStack;
-
-  for(unsigned int ui=0; ui<vBkg.size(); ui++){
-    Sample* s = vBkg.at(ui);
+  for(unsigned int uk=0; uk<vBkg.size(); uk++){
+    Sample* s = vBkg.at(uk);
     TH1F* h = new TH1F("h",variable.c_str(),50,0,2000);
     TTree* t = s->tree;
-    std::string drawstring = variable+">>h";
-    t->Draw(drawstring.c_str());
+    //std::string drawstring = variable+">>h";
+    //std::cout<<"drawstring is: "<<drawstring<<std::endl;
+    //t->Draw(drawstring.c_str());
+    t->Project("h",variable.c_str());
     //scale by weight
     h->Scale(s->weight);
     h->SetFillColor(s->color);
-    tStack.Add(h);
+    std::cout<<"mean of: "<<variable<<" in sample "<<vBkg.at(uk)->name<<" is: "<<h->GetMean()<<std::endl;
+    assert(h);
+    tStack->Add(h);
+    //delete h;
   }
 
-  tStack.Draw();
+  std::cout<<"about to draw tstack"<<std::endl;
+  tStack->Draw("HIST");
+  c1->Update();
+  std::cout<<"drew tstack"<<std::endl;
 
-  return c;
+  std::string pdfname = variable+"_"+(vBkg[0]->cutname)+".pdf";
+  std::cout<<"pdfname: "<<pdfname<<std::endl;
+  c1->Print(pdfname.c_str());
+  delete c1;
 
 }
