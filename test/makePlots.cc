1b9f50fba39246593ca38b84e4019ba1b667a63b
Clint <clint.a.richardson@gmail.com>
2015-06-10 04:19:57 -0500

    temporary state, added function to get vector of background samples, but I also want to add a function to get vector of histograms for a variable given a variable name and use those draw with

--- test/makePlots.cc
+++ test/makePlots.cc
@@ -12,11 +12,12 @@
 #include "../plugins/Variable.cc"
 #include "TCanvas.h"
 #include "TLegend.h"
+#include "TLatex.h"
 
-
-std::vector<Sample*> getSampleVec(std::string cut,float lumi);
+std::vector<Sample*> getBkgSampleVec(std::string cut,float lumi);
+std::vector<Sample*> getSigSampleVec(std::string cut,float lumi);
 std::vector<Variable*> getVariableVec();
-void DrawAndSave(Variable* Var, std::vector<Sample*> vBkg);
+void DrawAndSave(Variable* Var, std::vector<Sample*> vBkg, std::vector<Sample*> vSig);
 //TCanvas* DrawCanvas(std::string variable, std::vector<Sample*> vBkg);
   
 void makePlots(){
@@ -29,19 +30,19 @@ void makePlots(){
   float lumi = 5.0; //fb^-1
 
 
-  std::vector<Sample*> vBkgSamples = getSampleVec("ssdl", lumi);
+  std::vector<Sample*> vBkgSamples = getBkgSampleVec("ssdl", lumi);
+  std::vector<Sample*> vSiggSamples = getSigSampleVec("ssdl", lumi);
+  
   std::vector<Variable*> vVariables = getVariableVec();
 
   for(unsigned int i=0; i<vVariables.size();i++){
-    DrawAndSave(vVariables.at(i),vBkgSamples);
+    DrawAndSave(vVariables.at(i),vBkgSamples,vSigSamples);
   }
 
 
 }
 
-std::vector<Sample*> getSampleVec(std::string cut, float lumi){
-
-  
+std::vector<Sample*> getBkgSampleVec(std::string cut, float lumi){  
 
    //setup info for list of samples, xsec and events run
   std::vector<std::string> vBkgNames;
@@ -59,7 +60,7 @@ std::vector<Sample*> getSampleVec(std::string cut, float lumi){
     vWeights.push_back( lumi * 1000 * ( vXsec.at(ui) / vNEvts.at(ui) ) ); //factor of 1000 to convert lumi to pb^-1
   }
 
-
+  //now make samples and add to vector
   std::vector<Sample*> vSample;
   TFile* ttfile = new TFile("TTZ.root");
   Sample* ttSample = new Sample(vBkgNames.at(0),ttfile, vWeights.at(0),vXsec.at(0),cut,kRed+2);
@@ -74,9 +75,48 @@ std::vector<Sample*> getSampleVec(std::string cut, float lumi){
   Sample* wjSample = new Sample(vBkgNames.at(3),wjfile, vWeights.at(3),vXsec.at(3),cut,kGreen+2);
   vSample.push_back(wjSample);
 
-
-
   return vSample;
+
+}
+
+std::vector<Sample*> get SigSample(std::string cut, float lumi){
+  //make names vector
+  std::vector<std::string> vSigNames;
+  vSigNames.push_back("LH X53X53 M-700"); vSigNames.push_back("RH X53X53 M-700"); vSigNames.push_back("LH X53X53 M-1000"); vSigNames.push_back("LH X53X53 M-1000"); vSigNames.push_back("RH X53X53 M-1300"); vSigNames.push_back("RH X53X53 M-1300");
+  //make x-sec vector - NEED TO FIX THESE VALUES************************************************
+  std::vector<float> vXsec;
+  vXsec.push_back(1);vXsec.push_back(1);vXsec.push_back(1);vXsec.push_back(1);vXsec.push_back(1);vXsec.push_back(1);
+  //make vector for number of events ran to get weight
+  std::vector<int> vNEvts;
+  vNEvts.push_back(19600);vNEvts.push_back(16200);vNEvts.push_back(16600);vNEvts.push_back(20000);vNEvts.push_back(18800);vNEvts.push_back(19000);
+  
+
+  //vector to hold weights
+  std::vector<float> vWeights;
+  for(std::vector::size_type i=0; i<vXsec.size();i++){
+    vWeights.push_back( lumi * 1000 * ( vXsec.at(ui) / vNEvts.at(ui) ) ); //factor of 1000 to convert lumi to pb^-1
+  }
+
+  std::vector<Sample*> vSigSamples;
+
+  TFile* x53x53700Rfile = new TFile("X53X53m700RH.root");
+  Sample* x53x53m700R = new Sample(vSigNames.at(0),x53x53700Rfile,vWeights.at(0),vXsec.at(0),kGreen,2);
+  TFile* x53x53700Lfile = new TFile("X53X53m700LH.root");
+  Sample* x53x53m700L = new Sample(vSigNames.at(1),x53x53700Lfile,vWeights.at(1),vXsec.at(1),kGreen,1);
+
+  TFile* x53x531000Rfile = new TFile("X53X53m1000RH.root");
+  Sample* x53x53m1000R = new Sample(vSigNames.at(0),x53x531000Rfile,vWeights.at(0),vXsec.at(0),kBlack,2);
+  vSigSamples.push_back(x53x53m1000R);
+  TFile* x53x531000Lfile = new TFile("X53X53m1000LH.root");
+  Sample* x53x53m1000L = new Sample(vSigNames.at(1),x53x531000Lfile,vWeights.at(1),vXsec.at(1),kBlack,1);
+  vSigSamples.push_back(x53x53m1000L);
+
+  TFile* x53x531300Rfile = new TFile("X53X53m1300RH.root");
+  Sample* x53x53m1300R = new Sample(vSigNames.at(0),x53x531300Rfile,vWeights.at(0),vXsec.at(0),kBlue,2);
+  TFile* x53x531300Lfile = new TFile("X53X53m1300LH.root");
+  Sample* x53x53m1300L = new Sample(vSigNames.at(1),x53x531300Lfile,vWeights.at(1),vXsec.at(1),kBlue,1);
+
+  return vSigSamples;
 
 }
 
@@ -84,13 +124,38 @@ std::vector<Variable*> getVariableVec(){
 
   std::vector<Variable*> vVar;
 
-  Variable* lep1pt = new Variable("Lep1Pt",60,0,600);
+  Variable* lep1pt = new Variable("Lep1Pt",60,0,600,"p_{T} (GeV)","N_{Events}");
   vVar.push_back(lep1pt);
-  Variable* lep2pt = new Variable("Lep2Pt",60,0,600);
+  Variable* lep1eta = new Variable("Lep1Eta",25,-5,5,"#eta","N_{Events}");
+  vVar.push_back(lep1eta);
+  Variable* lep1phi = new Variable("Lep1Phi",20,-3.5,3.5,"#phi","N_{Events}");
+  vVar.push_back(lep1phi);
+
+  Variable* lep2pt = new Variable("Lep2Pt",60,0,600,"p_{T} (GeV)","N_{Events}");
   vVar.push_back(lep2pt);
-  Variable* ak4ht = new Variable("AK4HT",60,0,3000);
+  Variable* lep2eta = new Variable("Lep2Eta",25,-5,5,"#eta","N_{Events}");
+  vVar.push_back(lep2eta);
+  Variable* lep2phi = new Variable("Lep2Phi",20,-3.5,3.5,"#phi","N_{Events}");
+  vVar.push_back(lep2phi);
+
+  Variable* ak4jet1pt = new Variable("AK4Jet1Pt",60,0,600,"p_{T} (GeV)","N_{Events}");
+  vVar.push_back(ak4jet1pt);
+  Variable* ak4jet1eta = new Variable("AK4Jet1Eta",25,-5,5,"#eta","N_{Events}");
+  vVar.push_back(ak4jet1eta);
+  Variable* ak4jet1phi = new Variable("AK4Jet1Phi",20,-3.5,3.5,"#phi","N_{Events}");
+  vVar.push_back(ak4jet1phi);
+
+  Variable* ak4jet2pt = new Variable("AK4Jet2Pt",60,0,600,"p_{T} (GeV)","N_{Events}");
+  vVar.push_back(ak4jet2pt);
+  Variable* ak4jet2eta = new Variable("AK4Jet2Eta",25,-5,5,"#eta","N_{Events}");
+  vVar.push_back(ak4jet2eta);
+  Variable* ak4jet2phi = new Variable("AK4Jet2Phi",20,-3.5,3.5,"#phi","N_{Events}");
+  vVar.push_back(ak4jet2phi);
+
+
+  Variable* ak4ht = new Variable("AK4HT",60,0,3000,"H_{T} (GeV)","N_{Events}");
   vVar.push_back(ak4ht);
-  Variable* nak4jets = new Variable("nAK4Jets",15,0,15);
+  Variable* nak4jets = new Variable("nAK4Jets",17,0,17,"N_{AK4 Jets}","N_{Events}");
   vVar.push_back(nak4jets);
 
   return vVar;
@@ -98,11 +163,18 @@ std::vector<Variable*> getVariableVec(){
 }
 
 
-void DrawAndSave(Variable* var, std::vector<Sample*> vBkg){
+void DrawAndSave(Variable* var, std::vector<Sample*> vBkg, std::vector<Sample*> vSig){
 
   TCanvas* c1 = new TCanvas("c1","c1");
 
   c1->SetLogy();
+
+  TLatex* cmstex = new TLatex();
+  cmstex->SetNDC();
+  cmstex->SetTextSize(0.04);
+  TLatex* lumitex = new TLatex();
+  lumitex->SetNDC();
+  lumitex->SetTextSize(0.04);
 
   THStack* tStack = new THStack("tStack","");
   TLegend* leg = new TLegend(0.65,0.6,0.9,0.9);
@@ -118,21 +190,42 @@ void DrawAndSave(Variable* var, std::vector<Sample*> vBkg){
     t->Project("h",(var->name).c_str());
     //scale by weight
     h->Scale(s->weight);
+    //aesthetics
     h->SetFillColor(s->color);
+    h->GetXaxis()->SetTitle(var->Xaxis.c_str());
+    h->GetYaxis()->SetTitle(var->Yaxis.c_str());
     //add to legend
     leg->AddEntry(h,(vBkg.at(uk)->name).c_str(),"f");
     assert(h);
     tStack->Add(h);
-
   }
 
 
   tStack->Draw("HIST");
+  tStack->GetXaxis()->SetTitle(var->Xaxis.c_str());
+  tStack->GetXaxis()->CenterTitle();
+  //tStack->GetXaxis()->SetTitleSize(0.015);
+  tStack->GetYaxis()->SetTitle(var->Yaxis.c_str());
+  tStack->GetYaxis()->CenterTitle();
+  //tStack->GetYaxis()->SetTitleSize(0.015);
+  c1->RedrawAxis();
   c1->Update();
 
-  leg->Draw("same");
+  //draw signal:
+  for(std::vector::size_type i=0;i<vSig.size();i++){
+    Sample* s = vSig.at(i);
+    TH1F* h = new TH1F("h",(var->name).c_str(), var->nbins, var->xmin, var->xmax);
+    TTree* t = s->tree;
+    
+  }
 
-  std::string pdfname = (var->name)+"_"+(vBkg[0]->cutname)+".pdf";
+
+  //draw legend, cms and lumi
+  leg->Draw("same");
+  cmstex->DrawLatex(0.15,0.96,"CMS Simulation Preliminary");
+  lumitex->DrawLatex(0.65,0.96,"5.0 fb^{-1} (13 TeV)");
+
+  std::string pdfname = "./plots/"+(var->name)+"_"+(vBkg[0]->cutname)+".pdf";
 
   c1->Print(pdfname.c_str());
   delete c1;
