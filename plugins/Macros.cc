1b84463bac9ce7313660625a3fa4940bd9d38d3d
Clint <clint.a.richardson@gmail.com>
2015-06-11 03:17:34 -0500

    fixed signal BR

--- plugins/Macros.cc
+++ plugins/Macros.cc
@@ -54,7 +54,7 @@ std::vector<Sample*> getSigSampleVec(std::string cut, float lumi){
   //make x-sec vector - NEED TO FIX THESE VALUES************************************************
   std::vector<float> vXsec;
   //br is OR of either side decaying to ssdl. BR(ssdl) for one side = BR(W->enu) OR BR(W->munu) **2 where the square comes from AND requiring both to decay leptonically
-  float BRssdl= 2*( pow((.1063 + .1071 + .1138*(.1741+.1783)),2));
+  float BRssdl= 2*( pow((.1063 + .1071 + .1138),2));
   vXsec.push_back(0.442*BRssdl);vXsec.push_back(0.442*BRssdl);vXsec.push_back(0.0427*BRssdl);vXsec.push_back(0.0427*BRssdl);vXsec.push_back(0.00618*BRssdl);vXsec.push_back(0.00618*BRssdl);
   //make vector for number of events ran to get weight
   std::vector<int> vNEvts;
