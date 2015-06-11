79108a7d8940e0c352959c0eae841592b8371aa1
Clint <clint.a.richardson@gmail.com>
2015-06-11 03:02:43 -0500

    updated includes to be consistent with macros.cc

--- plugins/Sample.cc
+++ plugins/Sample.cc
@@ -3,11 +3,13 @@
 #include <iostream>
 #include <string>
 #include "TH1.h"
+#include "/uscms_data/d3/clint/using_git/T53/ljmet/CMSSW_7_3_0/src/AnalysisCode/ThirteenTeVX53/plugins/Variable.cc"
+#include <assert.h>
 
 class Sample {
 
 public:
-  Sample(std::string n, TFile* f, float w, float xs,std::string cut,int c);
+  Sample(std::string n, TFile* f, float w, float xs,std::string cut,int c, int ls=0);
   ~Sample();
 
   std::string name;
@@ -17,14 +19,49 @@ public:
   float xsec;
   std::string cutname;
   int color;
+  int linestyle;
+  TH1F* hist;
+  void setHist(Variable* var);
 };
   
-Sample::~Sample(){};
+Sample::~Sample(){
+  delete hist;
+  delete tree;
+  delete file;
+};
 
-Sample::Sample(std::string n, TFile* f, float w, float xs, std::string cut, int c):
-  name(n),file(f),weight(w), xsec(xs),cutname(cut), color(c)
+Sample::Sample(std::string n, TFile* f, float w, float xs, std::string cut, int c, int ls):
+  name(n),file(f),weight(w), xsec(xs),cutname(cut), color(c), linestyle(ls)
   
 {
   std::string tname = "tEvts_"+cutname;
   tree = (TTree*) file->Get(tname.c_str());
-}    
+
+  hist = new TH1F();
+
+
+};
+
+void Sample::setHist(Variable* var){
+
+  //reset the hist
+  hist->Reset();
+  //set name
+  std::string histname = "h_"+var->name+"_"+name;
+  std::cout<<"setting histname to: "<<histname<<std::endl;
+  hist->SetName(histname.c_str());
+  hist->SetFillColor(color);
+  //now draw variable into it
+  std::cout<<"about to project ttree for sample name "<<name<<std::endl;
+  tree->Project(histname.c_str(),(var->name).c_str());
+  std::cout<<"finished projecting ttree for sample "<<name<<" and mean is now: "<<hist->GetMean()<<std::endl;  
+  //now scale by weight
+  hist->Scale( weight);
+  std::cout<<"scaled weight for sample name "<<name<<std::endl;
+  //now set axis labels
+  hist->GetXaxis()->SetTitle(var->Xaxis.c_str());
+  hist->GetYaxis()->SetTitle(var->Yaxis.c_str());
+  hist->SetLineWidth(linestyle);
+
+}
+
--- plugins/Variable.cc
+++ plugins/Variable.cc
@@ -4,20 +4,21 @@
 class Variable {
 
 public:
-  Variable(std::string n, int nb, float xmi, float xma);
+  Variable(std::string n, int nb, float xmi, float xma,std::string Xax,std::string Yax);
   ~Variable();
 
   std::string name;
   int nbins;
   float xmin;
   float xmax;
-
+  std::string Xaxis;
+  std::string Yaxis;
 
 };
 
 Variable::~Variable(){};
 
-Variable::Variable(std::string n, int nb, float xmi, float xma):
-  name(n),nbins(nb),xmin(xmi),xmax(xma)
+Variable::Variable(std::string n, int nb, float xmi, float xma,std::string Xax, std::string Yax):
+  name(n),nbins(nb),xmin(xmi),xmax(xma),Xaxis(Xax),Yaxis(Yax)
 {
 };
