71c8c979da7f71f333887a8d74fc1b3df2bdf5f6
Clint <clint.a.richardson@gmail.com>
2015-06-15 16:32:56 -0500

    adds some needed lines for latex compiling as well as aesthetic improvements

--- test/makeTables.cc
+++ test/makeTables.cc
@@ -7,7 +7,7 @@
 #include "TTree.h"
 
 std::string tableHeader(std::vector<std::string> vC, CutClass* c);
-std::stringstream printTable(std::vector<CutClass*> vCC, std::vector<std::string> vCS, int nmu);
+std::stringstream printTable(std::vector<CutClass*> vCC, std::vector<std::string> vCS, int nmu,bool sig);
 
 void makeTables(){
 
@@ -31,14 +31,17 @@ void makeTables(){
     std::cout<<vCutString.at(i)<<std::endl;
   }
 
+  bool Bkg=false;
+  bool Sig=true;
+
   for(int nmu=-1; nmu<3; nmu++){
     //now make a vector of cutClass for bkg
     std::vector<CutClass*> vCutBkg = getCutClassVector(vBkg,vCutString,nmu);
     //now make a vector of cutClass for sig
     std::vector<CutClass*> vCutSig = getCutClassVector(vSig,vCutString,nmu);
     //now print background table
-    outfile<<(printTable(vCutBkg,vCutString,nmu)).str();
-    outfile<<(printTable(vCutSig,vCutString,nmu)).str();
+    outfile<<(printTable(vCutBkg,vCutString,nmu,Bkg)).str();
+    outfile<<(printTable(vCutSig,vCutString,nmu,Sig)).str();
 
   }
 
@@ -49,11 +52,11 @@ std::string tableHeader(std::vector<std::string> vC, CutClass* cC){
 
   std::stringstream str;
 
-  str<<"\\begin{table}\n"<<"\\centering\n"<<"\\begin{tabular}|";
+  str<<"\\begin{table}\n"<<"\\centering\n"<<"\\begin{tabular}{|";
   for(size_t i =0; i< (vC.size()+1); i++){
     str<<"c|";
   }
-  str<<"\n";
+  str<<"}\n\\hline\\hline\n";
 
   str<<"Sample";
   for(size_t i =0; i< (cC->cutname).size(); i++){
@@ -65,25 +68,42 @@ std::string tableHeader(std::vector<std::string> vC, CutClass* cC){
   return str.str();
 };
 
-std::stringstream printTable(std::vector<CutClass*> vCC, std::vector<std::string> vCS, int nmu){
+std::stringstream printTable(std::vector<CutClass*> vCC, std::vector<std::string> vCS, int nmu,bool sig){
 
   std::stringstream tablestring;
 
   //caption
   std::string caption;
-  if(nmu==-1) caption = "Event Count for all channels combined";
-  if(nmu==0) caption = "Event Count for di-electron channel";
-  if(nmu==1) caption = "Event Count for electron-muon channel";
-  if(nmu==2) caption = "Event Count for di-muon channel";
+  if(sig){
+    if(nmu==-1) caption = "Expected Number of Signal Events vs. Analysis Cut for all channels combined";
+    if(nmu==0) caption = "Expected Number of Signal Events vs. Analysis Cut for di-electron channel";
+    if(nmu==1) caption = "Expected Number of Signal Events vs. Analysis Cut for electron-muon channel";
+    if(nmu==2) caption = "Expected Number of Signal Events vs. Analysis Cut for di-muon channel";
+  }
+  else{
+    if(nmu==-1) caption = "Expected Number of Background Events vs. Analysis Cut for all channels combined";
+    if(nmu==0) caption = "Expected Number of Background Events vs. Analysis Cut for di-electron channel";
+    if(nmu==1) caption = "Expected Number of Background Events vs. Analysis Cut for electron-muon channel";
+    if(nmu==2) caption = "Expected Number of Background Events vs. Analysis Cut for di-muon channel";
+  }
+
 
   //label
   std::string label;
-  if(nmu==-1) label = "\\label{tab:ExpEvtsAll}";
-  if(nmu==0) label = "\\label{tab:ExpEvtsElEl}";
-  if(nmu==1) label = "\\label{tab:ExpEvtsElMu}";
-  if(nmu==2) label = "\\label{tab:ExpEvtsMuMu}";
+  if(sig){
+    if(nmu==-1) label = "\\label{tab:ExpSigEvtsAll}";
+    if(nmu==0) label = "\\label{tab:ExpSigEvtsElEl}";
+    if(nmu==1) label = "\\label{tab:ExpSigEvtsElMu}";
+    if(nmu==2) label = "\\label{tab:ExpSigEvtsMuMu}";
+  }
+  else{
+    if(nmu==-1) label = "\\label{tab:ExpBkgEvtsAll}";
+    if(nmu==0) label = "\\label{tab:ExpBkgEvtsElEl}";
+    if(nmu==1) label = "\\label{tab:ExpBkgEvtsElMu}";
+    if(nmu==2) label = "\\label{tab:ExpBkgEvtsMuMu}";
+  }
 
-  tablestring<<tableHeader(vCS, vCC.at(0));  
+  tablestring<<tableHeader(vCS, vCC.at(0)); tablestring<<"\\hline \n";
   for(size_t i=0; i < vCC.size(); i++){
     tablestring<<vCC.at(i)->samplename;
     for(size_t j =0; j < (vCC.at(i)->nEvents).size(); j++){
@@ -91,7 +111,7 @@ std::stringstream printTable(std::vector<CutClass*> vCC, std::vector<std::string
     }
     tablestring<<" \\\\"<<std::endl;
   }
-  tablestring<<"\\end{tabular} \n"<<"\\caption{"<<caption<<"}\n"<<label<<'\n'<<"\\end{table} \n\n";
+  tablestring<<"\\hline \n\\end{tabular} \n"<<"\\caption{"<<caption<<"}\n"<<label<<'\n'<<"\\end{table} \n\n";
 
   return tablestring;
  
