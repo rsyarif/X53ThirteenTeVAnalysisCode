3ddd0e7c8982cab51f5b605a35eca380491b338c
Clint <clint.a.richardson@gmail.com>
2015-08-25 13:06:22 -0500

    first commit

---
+++ Limits/makeLimitCards.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "TFile.h"
+#include "TTree.h"
+#include "TH1.h"
+#include <vector>
+#include "TLorentzVector.h"
+#include "TChain.h"
+#include "../interface/TreeReader.h"
+#include <stdio.h>
+#include <assert.h>
+#include <map>
+#include <string>
+#include <sstream> 
+#include "../plugins/Macros.cc"
+
+int main(int argc, char* argv[]){
+
+
+
+  return 0;
+}
