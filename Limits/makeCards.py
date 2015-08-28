b884214e2c7564f77b40a2b68baccba563ae1bc8
Clint <clint.a.richardson@gmail.com>
2015-08-28 09:53:21 -0500

    first commit

---
+++ Limits/makeCards.py
@@ -0,0 +1,26 @@
+#/usr/bin/python
+
+import os
+
+masses = [700,900,1000,1100,1300]
+chiralities = ['RH','LH']
+
+theta=True
+
+for mass in masses:
+    for chi in chiralities:
+        for i in range(-1,3):
+            for l1 in range(0,11):
+                lep1=l1*10
+                for l2 in range(0,11):
+                    lep2=l1*10
+                    for ht in range(0,11):
+                        htcut=50*ht
+                        if theta:
+                            command="./makeLimitCards.o %i %s %i %i %i %i theta" % (mass,chi,i,lep1,lep2,htcut)
+                            #print command
+                            os.system(command)
+                        else:
+                            command="./makeLimitCards.o %i %s %i %i %i %i higgs" % (mass,chi,i,lep1,lep2,htcut)
+                            os.system(command)
+                        
