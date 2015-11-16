a30306f8ccf24a39365e96bdbd6441d4d9ccda88
Clint <clint.a.richardson@gmail.com>
2015-11-16 07:44:14 -0600

    updated

--- Limits/makeCards.py
+++ Limits/makeCards.py
@@ -2,21 +2,21 @@
 
 import os
 
-masses = [700,800,900,1000,1100,1300,1400,1500]
+masses = [700,800,900,1000,1100,1200,1300,1400,1500]
 chiralities = ['RH','LH']
 
 theta=True
 
 for mass in masses:
     for chi in chiralities:
-        for i in range(-1,0):
-            for nc in range(6,7):
+        for i in range(-1,3):
+            for nc in range(5,6):
                 for l2 in range(0,1):
                     lep2=l2*10
                     for l1 in range(l2+1,2):
                         lep1=l1*10
-                        for ht in range(2,3):
-                            htcut=50*ht
+                        for ht in range(0,2):
+                            htcut=100*ht
                             if theta:
                                 command="./makeLimitCards.o %i %s %i %i %i %i %i theta" % (mass,chi,i,lep1,lep2,htcut,nc)
                                 print command
