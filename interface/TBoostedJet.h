c865c7695751effc3de77b985842f5f81b8be7c5
Clint <clint.a.richardson@gmail.com>
2016-04-26 07:18:51 -0500

    adding boosted jet class

---
+++ interface/TBoostedJet.h
@@ -0,0 +1,48 @@
+#ifndef TBoostedJet_h
+#define TBoostedJet_h
+
+#include <TLorentzVector.h>
+#include <cmath>
+#include <iostream>
+#include "TJet.h"
+
+class TBoostedJet{
+ public:
+  TBoostedJet(double pttemp, double etatemp, double phitemp, double energytemp,double tM, double pM, double sdM, double fM, double t1, double t2, double t3, int nD);
+  //kinematics
+  double pt;
+  double eta;
+  double phi;
+  double energy;
+
+  double trimmedMass;
+  double prunedMass;
+  double softDropMass;
+  double filteredMass;
+
+  double tau1;
+  double tau2;
+  double tau3;
+
+  int nDaughters;
+
+  std::vector<double> subjetPt;
+  std::vector<double> subjetEta;
+  std::vector<double> subjetPhi;
+  std::vector<double> subjetBDisc;
+  std::vector<int> subjetBTag;
+  std::vector<double> subjetDeltaR;
+  std::vector<double> subjetMass;
+
+  TBoostedJet(const TBoostedJet& jet);
+  ~TBoostedJet();
+  TLorentzVector lv;
+
+  void setLV(){
+    lv.SetPxPyPzE(pt*cos(phi), pt*sin(phi), pt*TMath::SinH(eta), energy);
+  }
+
+  void AddSubJet(TJet* j);
+
+};
+#endif
---
+++ src/TBoostedJet.cc
@@ -0,0 +1,28 @@
+#include "../interface/TBoostedJet.h"
+
+TBoostedJet::TBoostedJet(double pttemp, double etatemp, double phitemp, double energytemp,double tM, double pM, double sdM, double fM, double t1, double t2, double t3, int nD) :
+  pt(pttemp),eta(etatemp),phi(phitemp),energy(energytemp),trimmedMass(tM),prunedMass(pM),softDropMass(sdM),filteredMass(fM),tau1(t1),tau2(t2),tau3(t3),nDaughters(nD)
+{
+  setLV();
+}
+
+TBoostedJet::TBoostedJet(const TBoostedJet &jet){
+  pt=jet.pt;
+  eta=jet.eta;
+  phi=jet.phi;
+  energy=jet.energy;
+  setLV();
+}
+
+TBoostedJet::~TBoostedJet(){}
+
+void TBoostedJet::AddSubJet(TJet* j){
+
+  subjetPt.push_back(j->pt);
+  subjetEta.push_back(j->eta);
+  subjetPhi.push_back(j->phi);
+  subjetBDisc.push_back(j->bDisc);
+  subjetDeltaR.push_back(j->dR);
+  subjetMass.push_back(j->mass);
+
+}
