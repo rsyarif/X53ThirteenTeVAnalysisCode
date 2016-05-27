81fcc2a1883ac54101ab4282312e52962df175d8
Clint <clint.a.richardson@gmail.com>
2016-05-27 06:50:04 -0500

    added miniIso loose

--- src/TMuon.cc
+++ src/TMuon.cc
@@ -17,6 +17,16 @@ bool TMuon::cutBasedLoose(){
   return true;
 }
 
+bool TMuon::cutBasedLooseMiniIso(){
+  //  if (pt < 20)              return false;
+  if(fabs(eta)>2.4)          return false;
+  if(!PFMuon)                return false;
+  if(!(Tracker || Global))   return false;
+  //added iso cut
+  if(miniIso > 0.4)           return false;
+  return true;
+}
+
 bool TMuon::cutBasedTight_NoIso(){
   if(fabs(eta)>2.4)         return false;
   if(!Global)               return false;
