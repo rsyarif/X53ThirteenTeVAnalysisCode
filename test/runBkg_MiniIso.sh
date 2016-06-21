854b983d8ef01ef4d4ace4685438b9bd5c6de8fb
Clint <clint.a.richardson@gmail.com>
2016-06-21 03:19:30 -0500

    adding simple run scripts

---
+++ test/runBkg_MiniIso.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+./X53Analyzer.o TTH MVATightRC CBTightMiniIso&
+./X53Analyzer.o TTW MVATightRC CBTightMiniIso&
+./X53Analyzer.o TTZ MVATightRC CBTightMiniIso&
+./X53Analyzer.o TTTT MVATightRC CBTightMiniIso;
+
+
+./X53Analyzer.o VH MVATightRC CBTightMiniIso&
+./X53Analyzer.o WZ MVATightRC CBTightMiniIso&
+./X53Analyzer.o ZZ MVATightRC CBTightMiniIso;
+
+./X53Analyzer.o WWW MVATightRC CBTightMiniIso&
+./X53Analyzer.o WWZ MVATightRC CBTightMiniIso&
+./X53Analyzer.o WZZ MVATightRC CBTightMiniIso;
+./X53Analyzer.o ZZZ MVATightRC CBTightMiniIso;
+#./X53Analyzer.o WWW MVATightRC CBTightMiniIso;
+
+./X53Analyzer.o WpWp MVATightRC CBTightMiniIso;
---
+++ test/runSig_MiniIso.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+./X53Analyzer.o X53X53m700LH_Inc MVATightRC CBTightMiniIso&
+./X53Analyzer.o X53X53m800LH_Inc MVATightRC CBTightMiniIso&
+./X53Analyzer.o X53X53m900LH_Inc MVATightRC CBTightMiniIso&
+./X53Analyzer.o X53X53m1000LH_Inc MVATightRC CBTightMiniIso;
+./X53Analyzer.o X53X53m1100LH_Inc MVATightRC CBTightMiniIso&
+./X53Analyzer.o X53X53m1200LH_Inc MVATightRC CBTightMiniIso&
+./X53Analyzer.o X53X53m1300LH_Inc MVATightRC CBTightMiniIso&
+./X53Analyzer.o X53X53m1400LH_Inc MVATightRC CBTightMiniIso;
+./X53Analyzer.o X53X53m1500LH_Inc MVATightRC CBTightMiniIso&
+./X53Analyzer.o X53X53m1600LH_Inc MVATightRC CBTightMiniIso&
+
+./X53Analyzer.o X53X53m700RH_Inc MVATightRC CBTightMiniIso&
+./X53Analyzer.o X53X53m800RH_Inc MVATightRC CBTightMiniIso;
+./X53Analyzer.o X53X53m900RH_Inc MVATightRC CBTightMiniIso&
+./X53Analyzer.o X53X53m1000RH_Inc MVATightRC CBTightMiniIso&
+./X53Analyzer.o X53X53m1100RH_Inc MVATightRC CBTightMiniIso&
+./X53Analyzer.o X53X53m1200RH_Inc MVATightRC CBTightMiniIso;
+./X53Analyzer.o X53X53m1300RH_Inc MVATightRC CBTightMiniIso&
+./X53Analyzer.o X53X53m1400RH_Inc MVATightRC CBTightMiniIso&
+./X53Analyzer.o X53X53m1500RH_Inc MVATightRC CBTightMiniIso&
+./X53Analyzer.o X53X53m1600RH_Inc MVATightRC CBTightMiniIso;
