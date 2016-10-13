af0e99fd5f13530e2cc2dac46123462415ea14ee
Clint <clint.a.richardson@gmail.com>
2016-10-13 13:17:46 -0500

    added output root tree

--- test/FakeRate_ModifiedLepPt.cc
+++ test/FakeRate_ModifiedLepPt.cc
@@ -79,8 +79,8 @@ int main(int argc, char* argv[]){
   //get correct file
   std::vector<std::string> filenames;
   if(argv1.find("Data")!=std::string::npos) {
-    if(MuonChannel) filenames.push_back("/eos/uscms/store/user/lpctlbsm/clint/FakeRate/25ns/July2/ljmet_trees/ljmet_FakeRate_Mu.root");
-    else if(!MuonChannel) filenames.push_back("/eos/uscms/store/user/lpctlbsm/clint/FakeRate/25ns/July2/ljmet_trees/ljmet_FakeRate_El.root");
+    if(MuonChannel) filenames.push_back("/eos/uscms/store/user/clint/FakeRate/25ns/Aug19/ljmet_trees/ljmet_FakeRate_Mu.root");
+    else if(!MuonChannel) filenames.push_back("/eos/uscms/store/user/clint/FakeRate/25ns/Aug19/ljmet_trees/ljmet_FakeRate_El.root");
   }
   else{
     //filenames.push_back("/eos/uscms/store/user/lpctlbsm/clint/Spring15/25ns/FakeRate/Oct08/ljmet_trees/ljmet_QCDHT100To200.root");
@@ -96,10 +96,27 @@ int main(int argc, char* argv[]){
   for(unsigned int i =0; i<filenames.size(); i++){
     
     TFile* fout = new TFile(outname.at(i).c_str(),"RECREATE");
+
+    //output tree
+    TTree* outTree = new TTree("FakeRate","FakeRate");
+    float LepPt_,LepEta_,LepPhi_,LepE_,LepMiniIso_,LepMinDR_,LepSusyIso_;
+    int LepIsLoose_,LepIsTight_,LepCharge_;
+    outTree->Branch("LepPt",&LepPt_);
+    outTree->Branch("LepEta",&LepEta_);
+    outTree->Branch("LepPhi",&LepPhi_);
+    outTree->Branch("LepE",&LepE_);
+    outTree->Branch("LepCharge",&LepCharge_);
+    outTree->Branch("LepMiniIso",&LepMiniIso_);
+    outTree->Branch("LepSusyIso",&LepSusyIso_);
+    outTree->Branch("LepIsLoose",&LepIsLoose_);
+    outTree->Branch("LepIsTight",&LepIsTight_);
+    outTree->Branch("LepMinDR",&LepMinDR_);
+
     //get tree reader to read in data
     TreeReader* tr= new TreeReader(filenames.at(i).c_str(),!data);
     TTree* t=tr->tree;
-    
+
+
     
     //initialize needed histograms
     TH1F* ptNumHist = new TH1F("ptNumHist","p_{T} of Tight Leptons",20,0,600);
@@ -117,6 +134,7 @@ int main(int argc, char* argv[]){
     //get number of entries and start event loop
     int nEntries = t->GetEntries();
     for(int ient=0; ient<nEntries; ient++){
+
       
       tr->GetEntry(ient);
       
@@ -134,9 +152,12 @@ int main(int argc, char* argv[]){
       std::vector<TLepton*> leptons = makeLeptons(tr->allMuons,tr->allElectrons,MuonChannel, ID);
 
       //veto on events with more than one lepton or no leptons
-      if((leptons.size()==0) || (leptons.size() >1) ) continue;
+      if((leptons.size()==0) || (leptons.size() >1) ) continue;      
       //now just take the lepton remaining
       TLepton* lep = leptons.at(0);
+      //skip if not the correct flavor
+      if(MuonChannel && !(leptons.at(0)->isMu)) continue;
+      if(!MuonChannel && !(leptons.at(0)->isEl)) continue;
       //skip if it has pt not between 25 and 35
       if( !(lep->pt < 35 && lep->pt>25)) continue;
       etaHist_all->Fill(lep->eta);
@@ -166,12 +187,12 @@ int main(int argc, char* argv[]){
       if(mt>25) continue; //change to Arams value
       etaHist_MT->Fill(lep->eta);      
       //search through jet collection to check for jet mass
-      bool Zveto = ZVetoCheck(lep,tr->allAK4Jets);
+      bool Zveto = ZVetoCheck(lep,tr->cleanedAK4Jets);
       if(Zveto) continue;
       etaHist_ZVeto->Fill(lep->eta);
 
       //check for away jet
-      bool awayJet = AwayJetCheck(lep,tr->allAK4Jets);
+      bool awayJet = AwayJetCheck(lep,tr->cleanedAK4Jets);
       if(!awayJet) continue;
       etaHist_AwayJet->Fill(lep->eta);
 
@@ -179,8 +200,29 @@ int main(int argc, char* argv[]){
       etaDenHist->Fill(lep->eta); ptDenHist->Fill(lep->pt);
       if(lep->Tight){etaNumHist->Fill(lep->eta);ptNumHist->Fill(lep->pt);}
 
+      //fill tree
+      LepPt_ = lep->pt;
+      LepEta_= lep->eta;
+      LepPhi_= lep->phi;
+      LepE_  = lep->energy;
+      LepCharge_ = lep->charge;
+      LepIsTight_ = (int) lep->Tight;
+      LepIsLoose_ = (int) lep->Loose;
+      LepMiniIso_ = lep->miniIso;
+      LepSusyIso_ = lep->susyIso;
+
+      //find minDR
+      float minDR=999;
+      for(unsigned int i=0; i< tr->cleanedAK4Jets.size();i++){
+	float drtemp = pow( pow(lep->eta - tr->cleanedAK4Jets.at(i)->eta, 2) + pow( lep->phi - tr->cleanedAK4Jets.at(0)->phi, 2) , 0.5);
+	if(drtemp<minDR) minDR = drtemp;
+      }
+      LepMinDR_ = minDR;
+      outTree->Fill();
+
     }//end event loop
     
+    fout->WriteTObject(outTree);
     fout->Append(ptNumHist);
     fout->Append(ptDenHist);
     fout->Append(etaNumHist);
@@ -210,122 +252,121 @@ std::vector<TLepton*> makeLeptons(std::vector<TMuon*> muons, std::vector<TElectr
 
   std::vector<TLepton*> Leptons;
 
-  if(Muons){
-    //fill with  muons
-    for(unsigned int uimu=0; uimu<muons.size(); uimu++){
-      TMuon* imu = muons.at(uimu);
-      TLepton* iLep = new TLepton(imu->pt,imu->eta,imu->phi,imu->energy,imu->charge);
-      if(ID=="CBTight"){
-	iLep->Tight=imu->cutBasedTight();
-	iLep->Loose=imu->cutBasedLoose();
-      }
-      else if(ID=="CBLoose"){
-	iLep->Tight=imu->cutBasedLoose();
-	iLep->Loose=true; //in case 'loose ID' is specified as 'tight', take any muon as loose ID
-      }
-      else if(ID=="CBTightMiniIso"){
-	iLep->Tight=imu->cutBasedTightMiniIso();
-	iLep->Loose=imu->cutBasedLooseMiniIso();
-      }
-      iLep->isMu = true;
-      iLep->isEl = false;
-      //only save if at least loose
-      if(iLep->Tight || iLep->Loose){
-	//apply pt cut
-	if(iLep->pt>20) Leptons.push_back(iLep);
-	//if(iLep->pt>25 && iLep->pt<35) Leptons.push_back(iLep);
-      }
+  //fill with  muons
+  for(unsigned int uimu=0; uimu<muons.size(); uimu++){
+    TMuon* imu = muons.at(uimu);
+    TLepton* iLep = new TLepton(imu->pt,imu->eta,imu->phi,imu->energy,imu->charge,imu->relIso,imu->miniIso,imu->susyIso);
+    if(ID=="CBTight"){
+      iLep->Tight=imu->cutBasedTight();
+      iLep->Loose=imu->cutBasedLoose();
+    }
+    else if(ID=="CBLoose"){
+      iLep->Tight=imu->cutBasedLoose();
+      iLep->Loose=true; //in case 'loose ID' is specified as 'tight', take any muon as loose ID
+    }
+    else if(ID=="CBTightMiniIso"){
+      iLep->Tight=imu->cutBasedTightMiniIso();
+      iLep->Loose=imu->cutBasedLooseMiniIso();
+    }
+    iLep->isMu = true;
+    iLep->isEl = false;
+    //only save if at least loose
+    if(iLep->Tight || iLep->Loose){
+      //apply pt cut
+      if(iLep->pt>20){ Leptons.push_back(iLep);}
+      //if(iLep->pt>25 && iLep->pt<35) Leptons.push_back(iLep);
     }
   }
 
-  else{
-    //fill with  electrons
-    for(unsigned int uiel=0; uiel<electrons.size(); uiel++){
-      TElectron* iel = electrons.at(uiel);
-      TLepton* iLep = new TLepton(iel->pt,iel->eta,iel->phi,iel->energy,iel->charge);
-      if(ID=="CBTight"){
-	iLep->Tight=iel->cutBasedTight25nsSpring15MC();
-	iLep->Loose=iel->cutBasedLoose25nsSpring15MC();
-      }
-      if(ID=="CBTightRC"){
-	iLep->Tight=iel->cutBasedTight25nsSpring15MCRC();
-	iLep->Loose=iel->cutBasedLoose25nsSpring15MCRC();
-      }
-      else if(ID=="CBLoose"){
-	iLep->Tight=iel->cutBasedLoose25nsSpring15MC();
-	iLep->Loose=true;
-      }
-      else if(ID=="MVATightNew"){
-	iLep->Tight=iel->mvaTightNew();
-	iLep->Loose=iel->mvaLooseNew();
-      }
-      else if(ID=="MVATightNewRC"){
-	iLep->Tight=iel->mvaTightNewRC();
-	iLep->Loose=iel->mvaLooseNewRC();
-      }
-      else if(ID=="MVATight"){
-	iLep->Tight=iel->mvaTightIso();
-	iLep->Loose=iel->mvaLooseIso();
-      }
-      else if(ID=="MVATightNoIso"){
-	iLep->Tight=iel->mvaTight();
-	iLep->Loose=iel->mvaLoose();
-      }
-      else if(ID=="MVALoose"){
-	iLep->Tight=iel->mvaLooseIso();
-	iLep->Loose=true;
-      }
-      else if(ID=="MVALooseNoIso"){
-	iLep->Tight=iel->mvaLoose();
-	iLep->Loose=true;
-      }
-      else if(ID=="MVATightCC"){
-	iLep->Tight=iel->mvaTightCCIso();
-	iLep->Loose=iel->mvaLooseCCIso();
-      }
-      else if(ID=="MVATightCCNoIso"){
-	iLep->Tight=iel->mvaTightCC();
-	iLep->Loose=iel->mvaLooseCC();
-      }
-      else if(ID=="MVALooseCC"){
-	iLep->Tight=iel->mvaLooseCCIso();
-	iLep->Loose=true;
-      }
-      else if(ID=="MVALooseCCNoIso"){
-	iLep->Tight=iel->mvaLooseCC();
-	iLep->Loose=true;
-      }
-      else if(ID=="MVATightRC"){
-	iLep->Tight=iel->mvaTightRCIso();
-	iLep->Loose=iel->mvaLooseRCIso();
-      }
-      else if(ID=="MVALooseRC"){
-	iLep->Tight=iel->mvaLooseRCIso();
-	iLep->Loose=true;
-      }
-      else if(ID=="SUSYTight"){
-	iLep->Tight=iel->susyTight();
-	iLep->Loose=iel->susyLoose();
-      }
-      else if(ID=="SUSYLoose"){
-	iLep->Tight=iel->susyLoose();
-	iLep->Loose=true;
-      }
-      else if(ID=="SUSYTightRC"){
-	iLep->Tight=iel->susyTightRC();
-	iLep->Loose=iel->susyLooseRC();
-      }      
 
-      iLep->isMu = false;
-      iLep->isEl = true;
-      //only save if at least loose
-      if(iLep->Tight || iLep->Loose){
-	//apply pt cut
-	//if(iLep->pt>25 && iLep->pt<35) Leptons.push_back(iLep);
-	if(iLep->pt>20) Leptons.push_back(iLep);
-      }
+
+
+  for(unsigned int uiel=0; uiel<electrons.size(); uiel++){
+    TElectron* iel = electrons.at(uiel);
+    TLepton* iLep = new TLepton(iel->pt,iel->eta,iel->phi,iel->energy,iel->charge,iel->relIsoEA,iel->miniIso,iel->susyIso);
+    if(ID=="CBTight"){
+      iLep->Tight=iel->cutBasedTight25nsSpring15MC();
+      iLep->Loose=iel->cutBasedLoose25nsSpring15MC();
+    }
+    if(ID=="CBTightRC"){
+      iLep->Tight=iel->cutBasedTight25nsSpring15MCRC();
+      iLep->Loose=iel->cutBasedLoose25nsSpring15MCRC();
+    }
+    else if(ID=="CBLoose"){
+      iLep->Tight=iel->cutBasedLoose25nsSpring15MC();
+      iLep->Loose=true;
+    }
+    else if(ID=="MVATightNew"){
+      iLep->Tight=iel->mvaTightNew();
+      iLep->Loose=iel->mvaLooseNew();
+    }
+    else if(ID=="MVATightNewRC"){
+      iLep->Tight=iel->mvaTightNewRC();
+      iLep->Loose=iel->mvaLooseNewRC();
+    }
+    else if(ID=="MVATight"){
+      iLep->Tight=iel->mvaTightIso();
+      iLep->Loose=iel->mvaLooseIso();
+    }
+    else if(ID=="MVATightNoIso"){
+      iLep->Tight=iel->mvaTight();
+      iLep->Loose=iel->mvaLoose();
+    }
+    else if(ID=="MVALoose"){
+      iLep->Tight=iel->mvaLooseIso();
+      iLep->Loose=true;
+    }
+    else if(ID=="MVALooseNoIso"){
+      iLep->Tight=iel->mvaLoose();
+      iLep->Loose=true;
+    }
+    else if(ID=="MVATightCC"){
+      iLep->Tight=iel->mvaTightCCIso();
+      iLep->Loose=iel->mvaLooseCCIso();
+    }
+    else if(ID=="MVATightCCNoIso"){
+      iLep->Tight=iel->mvaTightCC();
+      iLep->Loose=iel->mvaLooseCC();
+    }
+    else if(ID=="MVALooseCC"){
+      iLep->Tight=iel->mvaLooseCCIso();
+      iLep->Loose=true;
+    }
+    else if(ID=="MVALooseCCNoIso"){
+      iLep->Tight=iel->mvaLooseCC();
+      iLep->Loose=true;
+    }
+    else if(ID=="MVATightRC"){
+      iLep->Tight=iel->mvaTightRCIso();
+      iLep->Loose=iel->mvaLooseRCIso();
+    }
+    else if(ID=="MVALooseRC"){
+      iLep->Tight=iel->mvaLooseRCIso();
+      iLep->Loose=true;
+    }
+    else if(ID=="SUSYTight"){
+      iLep->Tight=iel->susyTight();
+      iLep->Loose=iel->susyLoose();
+    }
+    else if(ID=="SUSYLoose"){
+      iLep->Tight=iel->susyLoose();
+      iLep->Loose=true;
+    }
+    else if(ID=="SUSYTightRC"){
+      iLep->Tight=iel->susyTightRC();
+      iLep->Loose=iel->susyLooseRC();
+    }      
+    
+    iLep->isMu = false;
+    iLep->isEl = true;
+    //only save if at least loose
+    if(iLep->Tight || iLep->Loose){
+      //apply pt cut
+      //if(iLep->pt>25 && iLep->pt<35) Leptons.push_back(iLep);
+      if(iLep->pt>20){ Leptons.push_back(iLep);}
     }
   }
+
 
   return Leptons;
 
