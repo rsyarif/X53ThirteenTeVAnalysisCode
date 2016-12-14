d9997f302984d9c10f2d4232665269203c5f6062
Clint <clint.a.richardson@gmail.com>
2016-12-14 08:31:00 -0600

    adding id plotting script

---
+++ scripts/MuonID_plotter.cc
@@ -0,0 +1,51 @@
+#include "TFile.h"
+#include "TH2.h"
+#include "TCanvas.h"
+#include "TLatex.h"
+
+void MuonID_plotter(TString mcfile="TnP_MuonID_signal_mc_TightID_pt_eta.root", TString datafile = "TnP_MuonID_data_all_TightID_pt_abseta.root"){
+
+
+  TFile* fmc = new TFile(mcfile);
+  TFile* fdata = new TFile(datafile);
+
+  TCanvas* cmc = (TCanvas*) fmc->Get("tpTree/TightID_pt_eta/fit_eff_plots/abseta_pt_PLOT");
+  TCanvas* cdata = (TCanvas*) fdata->Get("tpTree/TightID_pt_abseta/fit_eff_plots/abseta_pt_PLOT");
+
+  TH2F* hmc = (TH2F*) cmc->GetPrimitive("abseta_pt_PLOT");
+  TH2F* hdata = (TH2F*) cdata->GetPrimitive("abseta_pt_PLOT");
+
+  TCanvas* c = new TCanvas();
+  hmc->SetTitle("Muon Tight ID Efficiency MC;|#eta|;p_{T}");
+  c->SetLogy();
+  hmc->Draw("text e colz");
+  TPaletteAxis *palettemc = (TPaletteAxis*)hmc->GetListOfFunctions()->FindObject("palette");
+  // the following lines moe the paletter. Choose the values you need for the position.
+  palettemc->SetX1NDC(0.91);
+  palettemc->SetX2NDC(0.96);
+  palettemc->SetY1NDC(0.1);
+  palettemc->SetY2NDC(0.9);
+  c->Modified();
+  c->Update();
+  c->Print("MuonTightID_Eff_MC.pdf");
+
+  hdata->SetTitle("Muon Tight ID Efficiency Data;|#eta|;p_{T}");
+  hdata->Draw("text e colz");
+  TPaletteAxis *palettedata = (TPaletteAxis*)hdata->GetListOfFunctions()->FindObject("palette");
+  // the following lines moe the paletter. Choose the values you need for the position.
+  palettedata->SetX1NDC(0.91);
+  palettedata->SetX2NDC(0.96);
+  palettedata->SetY1NDC(0.1);
+  palettedata->SetY2NDC(0.9);
+  c->Modified();
+  c->Update();
+  c->Print("MuonTightID_Eff_Data.pdf");
+
+  //TH2F* hSF = (TH2F*) hdata->Clone("hSF");
+  //hSF->Divide(hmc);
+  //hSF->SetTitle("Muon Tight ID Data/MC Scale Factor;|#eta|;p_{T}");
+  //hSF->Draw("text e colz");
+  //c->Print("MuonTightID_SF.pdf");
+		
+
+}
