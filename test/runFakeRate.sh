32f400c6db24acd52779c5a25c966c1ca3eb0fb7
Clint <clint.a.richardson@gmail.com>
2017-01-12 12:22:00 -0600

    adding useful scripts

---
+++ test/runFakeRate.sh
@@ -0,0 +1,18 @@
+#./FakeRate_ModifiedLepPt.o Data Muons CBTightMiniIso 2016B&
+#./FakeRate_ModifiedLepPt.o Data Muons CBTightMiniIso 2016C&
+#./FakeRate_ModifiedLepPt.o Data Muons CBTightMiniIso 2016D&
+./FakeRate_ModifiedLepPt.o Data Muons CBTightMiniIso 2016E&
+./FakeRate_ModifiedLepPt.o Data Muons CBTightMiniIso 2016F&
+./FakeRate_ModifiedLepPt.o Data Muons CBTightMiniIso 2016G&
+./FakeRate_ModifiedLepPt.o Data Muons CBTightMiniIso 2016H2&
+./FakeRate_ModifiedLepPt.o Data Muons CBTightMiniIso 2016H3;
+
+
+./FakeRate_ModifiedLepPt.o Data El MVATightRC 2016B&
+./FakeRate_ModifiedLepPt.o Data El MVATightRC 2016C&
+./FakeRate_ModifiedLepPt.o Data El MVATightRC 2016D&
+./FakeRate_ModifiedLepPt.o Data El MVATightRC 2016E&
+./FakeRate_ModifiedLepPt.o Data El MVATightRC 2016F&
+./FakeRate_ModifiedLepPt.o Data El MVATightRC 2016G&
+./FakeRate_ModifiedLepPt.o Data El MVATightRC 2016H2&
+./FakeRate_ModifiedLepPt.o Data El MVATightRC 2016H3;
---
+++ test/runNP.sh
@@ -0,0 +1,13 @@
+root -b -q -l 'np.cc("900")'
+root -b -q -l 'np.cc("1000")'
+root -b -q -l 'np.cc("1100")'
+root -b -q -l 'np.cc("1200")'
+root -b -q -l 'np.cc("1300")'
+
+root -b -q -l 'np_MuTightIso.cc("900")'
+root -b -q -l 'np_MuTightIso.cc("1000")'
+root -b -q -l 'np_MuTightIso.cc("1100")'
+root -b -q -l 'np_MuTightIso.cc("1200")'
+root -b -q -l 'np_MuTightIso.cc("1300")'
+
+
