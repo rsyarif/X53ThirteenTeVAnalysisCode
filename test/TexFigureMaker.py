63c18cb6455537342a8d220e81c036b20e2a92f9
Clint <clint.a.richardson@gmail.com>
2017-01-26 09:07:19 -0600

    updated to do by era plots as well

--- test/TexFigureMaker.py
+++ test/TexFigureMaker.py
@@ -2,9 +2,6 @@
 
 import os
 
-#get list of figures
-plots = os.listdir("plots")
-
 def printHeader(figName,f):
 #clearpage if staring channels anew
     if not figName.find("All")==-1:
@@ -27,7 +24,28 @@ def printFooter(figName,f):
     label="\\label{fig:"
     name=figName.split("_")
     label+=name[0]
-    label+="}\n"
+    #write caption
+    caption="\\caption{"
+    capVar = name[0]
+    capVar = capVar.replace('clean','')
+    capVar = capVar.replace('Clean','')
+    capVar = capVar.replace('Jet1', " Leading Jet ")
+    capVar = capVar.replace('Jet2', " sub-Leading Jet ")
+    capVar = capVar.replace('Lep1',' Leading Lepton ')
+    capVar = capVar.replace('Lep2',' sub-Leading Lepton ')
+    capVar = capVar.replace('Pt',' \\pt')
+    capVar = capVar.replace('Eta',' $\\eta$')
+    capVar = capVar.replace('Phi',' $\\phi$')
+    caption= caption+capVar+' for eras '
+    if figName.find('2016B-D')!=-1:
+        era = '2016B-D'
+    elif figName.find('2016E-H')!=-1:
+        era='2016E-H'
+    else:
+        era='2016B-H'
+    caption= caption+era+'}\n'
+    f.write(caption)
+    label=label+'-'+era+"}\n"
     f.write(label)
 #end figure
     f.write("\\end{figure}")
@@ -37,9 +55,27 @@ def printFooter(figName,f):
 #make outfile
 outfile=open("Figures.tex","w+")
 
+#get list of figures
+plots = os.listdir("plots_forPAS")
+plots.sort(key=lambda x:(not x.islower(), x))
+
+plotsFull=[]
+plotsBD = []
+plotsEH = []
+
 for plot in plots:
+    if plot.find('2016B-D')!=-1:
+        plotsBD.append(plot)
+    elif plot.find('2016E-H')!=-1:
+        plotsEH.append(plot)
+    else:
+        plotsFull.append(plot)
+
+for plot in plotsFull:
 #skip non pdfs
     if plot.find(".pdf")==-1:
+        continue
+    if plot.find("new")!=-1 or plot.find('New')!=-1 or plot.find('NW')!=-1 or plot.find('NTop')!=-1 or plot.find('X53Mass')!=-1:
         continue
     if not plot.find("All")==-1:
         printHeader(plot,outfile)
@@ -47,6 +83,33 @@ for plot in plots:
         printFigure(plot,outfile)
     else:
         printFooter(plot,outfile)
+
+for plot in plotsBD:
+#skip non pdfs
+    if plot.find(".pdf")==-1:
+        continue
+    if plot.find("new")!=-1 or plot.find('New')!=-1 or plot.find('NW')!=-1 or plot.find('NTop')!=-1 or plot.find('X53Mass')!=-1:
+        continue
+    if not plot.find("All")==-1:
+        printHeader(plot,outfile)
+    elif plot.find("MuMu")==-1:
+        printFigure(plot,outfile)
+    else:
+        printFooter(plot,outfile)
+
+for plot in plotsEH:
+#skip non pdfs
+    if plot.find(".pdf")==-1:
+        continue
+    if plot.find("new")!=-1 or plot.find('New')!=-1 or plot.find('NW')!=-1 or plot.find('NTop')!=-1 or plot.find('X53Mass')!=-1:
+        continue
+    if not plot.find("All")==-1:
+        printHeader(plot,outfile)
+    elif plot.find("MuMu")==-1:
+        printFigure(plot,outfile)
+    else:
+        printFooter(plot,outfile)
+
 
 outfile.close()
 
