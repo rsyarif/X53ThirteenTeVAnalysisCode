24fbfb3ee872921e0cea493f2fffaa7bb45fe796
Clint <clint.a.richardson@gmail.com>
2017-02-07 09:12:56 -0600

    fixed typos

--- scripts/Systematics/JetEnergy/jetEnergySys.cc
+++ scripts/Systematics/JetEnergy/jetEnergySys.cc
@@ -1,7 +1,7 @@
-#include <stdio>
 #include <iostream>
-#include <vector.h>
-#include <string.h>
+#include <iomanip> 
+#include <vector>
+#include <string>
 #include "TString.h"
 
 float getEvents(TTree* t,TString cut){
@@ -10,14 +10,14 @@ float getEvents(TTree* t,TString cut){
 }
 
 
-void printLine(std::string sampleName, TString filename){
+void printLine(std::string sample, TString filename){
 
 
-  TString nomcut = "MCWeight * (Lep1Pt>40 && nConst >=5 && (DilepMass>20) && ( (Channel!=0) ||(DilepMass<76.1 || DilepMass >106.1) ) && cleanAK4HT > 1200";
-  TString jesUpcut = "MCWeight * (Lep1Pt>40 && nConst >=5 && (DilepMass>20) && ( (Channel!=0) ||(DilepMass<76.1 || DilepMass >106.1) ) && cleanAK4HTScaleUp > 1200";
-  TString jesDowncut = "MCWeight * (Lep1Pt>40 && nConst >=5 && (DilepMass>20) && ( (Channel!=0) ||(DilepMass<76.1 || DilepMass >106.1) ) && cleanAK4HTScaleDown > 1200";
-  TString jerUpcut = "MCWeight * (Lep1Pt>40 && nConst >=5 && (DilepMass>20) && ( (Channel!=0) ||(DilepMass<76.1 || DilepMass >106.1) ) && cleanAK4HTSmearUp > 1200";
-  TString jerDowncut = "MCWeight * (Lep1Pt>40 && nConst >=5 && (DilepMass>20) && ( (Channel!=0) ||(DilepMass<76.1 || DilepMass >106.1) ) && cleanAK4HTSmearDown > 1200";
+  TString nomcut = "MCWeight * (Lep1Pt>40 && nConst >=5 && (DilepMass>20) && ( (Channel!=0) ||(DilepMass<76.1 || DilepMass >106.1) ) && cleanAK4HT > 1200)";
+  TString jesUpcut = "MCWeight * (Lep1Pt>40 && nConst >=5 && (DilepMass>20) && ( (Channel!=0) ||(DilepMass<76.1 || DilepMass >106.1) ) && cleanAK4HTScaleUp > 1200)";
+  TString jesDowncut = "MCWeight * (Lep1Pt>40 && nConst >=5 && (DilepMass>20) && ( (Channel!=0) ||(DilepMass<76.1 || DilepMass >106.1) ) && cleanAK4HTScaleDown > 1200)";
+  TString jerUpcut = "MCWeight * (Lep1Pt>40 && nConst >=5 && (DilepMass>20) && ( (Channel!=0) ||(DilepMass<76.1 || DilepMass >106.1) ) && cleanAK4HTSmearUp > 1200)";
+  TString jerDowncut = "MCWeight * (Lep1Pt>40 && nConst >=5 && (DilepMass>20) && ( (Channel!=0) ||(DilepMass<76.1 || DilepMass >106.1) ) && cleanAK4HTSmearDown > 1200)";
 
 
   TFile* f = new TFile(filename);
@@ -31,8 +31,8 @@ void printLine(std::string sampleName, TString filename){
   float jerupPct = 100*(jerup-nom)/nom;
   float jerdown = getEvents(t,jerDowncut);
   float jerdownPct = 100*(jerdown-nom)/nom;
-
-  std::cout<<sample<<" | "<<nom<<" |  "<<jesupPct<<" |  "<<jesdownPct<<" |  "<<jerupPct<<" |  "<<jerdownPct<<std::endl;
+  std::cout<<setprecision(2);
+  std::cout<<sample<<" |  "<<jesupPct<<" |  "<<jesdownPct<<" |  "<<jerupPct<<" |  "<<jerdownPct<<std::endl;
 
   delete t; delete f;
 
@@ -43,16 +43,16 @@ void jetEnergySys(){
 
   //print top line
   std::cout<<" Sample | JES Up | JES Down | JER Up | JER Down"<<std::endl;
-  printLine("TTZ","../../../test/TTZ_MuCBTightMiniIsoTight_ElMVA2016TightRC_2016E-H.root");
-  printLine("TTW","../../../test/TTW_MuCBTightMiniIsoTight_ElMVA2016TightRC_2016E-H.root");
-  printLine("TTH","../../../test/TTH_MuCBTightMiniIsoTight_ElMVA2016TightRC_2016E-H.root");
+  printLine("TTZ ","../../../test/TTZ_MuCBTightMiniIsoTight_ElMVA2016TightRC_2016E-H.root");
+  printLine("TTW ","../../../test/TTW_MuCBTightMiniIsoTight_ElMVA2016TightRC_2016E-H.root");
+  printLine("TTH ","../../../test/TTH_MuCBTightMiniIsoTight_ElMVA2016TightRC_2016E-H.root");
   printLine("TTTT","../../../test/TTTT_MuCBTightMiniIsoTight_ElMVA2016TightRC_2016E-H.root");
-  printLine("WZ","../../../test/WZ_MuCBTightMiniIsoTight_ElMVA2016TightRC_2016E-H.root");
-  printLine("ZZ","../../../test/ZZ_MuCBTightMiniIsoTight_ElMVA2016TightRC_2016E-H.root");
+  printLine("WZ  ","../../../test/WZ_MuCBTightMiniIsoTight_ElMVA2016TightRC_2016E-H.root");
+  printLine("ZZ  ","../../../test/ZZ_MuCBTightMiniIsoTight_ElMVA2016TightRC_2016E-H.root");
   printLine("W+W+","../../../test/WpWp_MuCBTightMiniIsoTight_ElMVA2016TightRC_2016E-H.root");
-  printLine("WWZ","../../../test/WWZ_MuCBTightMiniIsoTight_ElMVA2016TightRC_2016E-H.root");
-  printLine("WZZ","../../../test/WZZ_MuCBTightMiniIsoTight_ElMVA2016TightRC_2016E-H.root");
-  printLine("ZZZ","../../../test/ZZZ_MuCBTightMiniIsoTight_ElMVA2016TightRC_2016E-H.root");
+  printLine("WWZ ","../../../test/WWZ_MuCBTightMiniIsoTight_ElMVA2016TightRC_2016E-H.root");
+  printLine("WZZ ","../../../test/WZZ_MuCBTightMiniIsoTight_ElMVA2016TightRC_2016E-H.root");
+  printLine("ZZZ ","../../../test/ZZZ_MuCBTightMiniIsoTight_ElMVA2016TightRC_2016E-H.root");
 
   std::cout<<"***********************************************"<<std::endl;
   printLine("700 LH","../../../test/X53X53m700LH_Inc_MuCBTightMiniIsoTight_ElMVA2016TightRC_2016E-H.root");
@@ -60,7 +60,7 @@ void jetEnergySys(){
   printLine("800 LH","../../../test/X53X53m800LH_Inc_MuCBTightMiniIsoTight_ElMVA2016TightRC_2016E-H.root");
   printLine("800 RH","../../../test/X53X53m800RH_Inc_MuCBTightMiniIsoTight_ElMVA2016TightRC_2016E-H.root");
   printLine("900 LH","../../../test/X53X53m900LH_Inc_MuCBTightMiniIsoTight_ElMVA2016TightRC_2016E-H.root");
-  printLine("900 RH","../../../test/X53X53m000RH_Inc_MuCBTightMiniIsoTight_ElMVA2016TightRC_2016E-H.root");
+  printLine("900 RH","../../../test/X53X53m900RH_Inc_MuCBTightMiniIsoTight_ElMVA2016TightRC_2016E-H.root");
   printLine("1000 LH","../../../test/X53X53m1000LH_Inc_MuCBTightMiniIsoTight_ElMVA2016TightRC_2016E-H.root");
   printLine("1000 RH","../../../test/X53X53m1000RH_Inc_MuCBTightMiniIsoTight_ElMVA2016TightRC_2016E-H.root");
   printLine("1100 LH","../../../test/X53X53m1100LH_Inc_MuCBTightMiniIsoTight_ElMVA2016TightRC_2016E-H.root");
