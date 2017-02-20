e294a13cc57a96d50692e6b16dbaeb86201cfe0a
Clint <clint.a.richardson@gmail.com>
2017-02-20 14:24:17 -0600

    latest id

--- test/hadd.sh
+++ test/hadd.sh
@@ -1,7 +1,11 @@
-hadd Data_MuCBTightMiniIso_ElMVATightRC_2016B-D.root Data*2016B* Data*2016C* Data*2016D*
-hadd NonPromptData_MuCBTightMiniIso_ElMVATightRC_2016B-D.root NonPromptData*2016B* NonPromptData*2016C* NonPromptData*2016D*
-hadd ChargeMisID_MuCBTightMiniIso_ElMVATightRC_2016B-D.root ChargeMisID*2016B* ChargeMisID*2016C* ChargeMisID*2016D*
 
-hadd Data_MuCBTightMiniIso_ElMVATightRC_2016E-H.root Data*2016E* Data*2016F* Data*2016G* Data*2016H*
-hadd NonPromptData_MuCBTightMiniIso_ElMVATightRC_2016E-H.root NonPromptData*2016E* NonPromptData*2016F* NonPromptData*2016G* NonPromptData*2016H*
-hadd ChargeMisID_MuCBTightMiniIso_ElMVATightRC_2016E-H.root ChargeMisID*2016E* ChargeMisID*2016F* ChargeMisID*2016G* ChargeMisID*2016H*
+#### muon tight mini iso, electron tight mini iso
+
+hadd -f Data_MuCBTightMiniIsoTight_ElMVA2016TightRC_2016B-D.root Data*2016B*Mu*CBTightMiniIsoTight*MVA2016TightRC* Data*2016C*Mu*CBTightMiniIsoTight*MVA2016TightRC* Data*2016D*Mu*CBTightMiniIsoTight*MVA2016TightRC*
+hadd -f Data_MuCBTightMiniIsoTight_ElMVA2016TightRC_2016E-H.root Data*2016E*Mu*CBTightMiniIsoTight*MVA2016TightRC* Data*2016F*Mu*CBTightMiniIsoTight*MVA2016TightRC* Data*2016G*Mu*CBTightMiniIsoTight*MVA2016TightRC* Data*2016H*Mu*CBTightMiniIsoTight*MVA2016TightRC*
+
+hadd -f NonPromptData_MuCBTightMiniIsoTight_ElMVA2016TightRC_2016B-D.root NonPromptData*2016B*Mu*CBTightMiniIsoTight*MVA2016TightRC* NonPromptData*2016C*Mu*CBTightMiniIsoTight*MVA2016TightRC* NonPromptData*2016D*Mu*CBTightMiniIsoTight*MVA2016TightRC*
+hadd -f NonPromptData_MuCBTightMiniIsoTight_ElMVA2016TightRC_2016E-H.root NonPromptData*2016E*Mu*CBTightMiniIsoTight*MVA2016TightRC* NonPromptData*2016F*Mu*CBTightMiniIsoTight*MVA2016TightRC* NonPromptData*2016G*Mu*CBTightMiniIsoTight*MVA2016TightRC* NonPromptData*2016H*Mu*CBTightMiniIsoTight*MVA2016TightRC*
+
+hadd -f ChargeMisID_MuCBTightMiniIsoTight_ElMVA2016TightRC_2016B-D.root ChargeMisID*2016B*Mu*CBTightMiniIsoTight*MVA2016TightRC* ChargeMisID*2016C*Mu*CBTightMiniIsoTight*MVA2016TightRC* ChargeMisID*2016D*Mu*CBTightMiniIsoTight*MVA2016TightRC*
+hadd -f ChargeMisID_MuCBTightMiniIsoTight_ElMVA2016TightRC_2016E-H.root ChargeMisID*2016E*Mu*CBTightMiniIsoTight*MVA2016TightRC* ChargeMisID*2016F*Mu*CBTightMiniIsoTight*MVA2016TightRC* ChargeMisID*2016G*Mu*CBTightMiniIsoTight*MVA2016TightRC* ChargeMisID*2016H*Mu*CBTightMiniIsoTight*MVA2016TightRC*
