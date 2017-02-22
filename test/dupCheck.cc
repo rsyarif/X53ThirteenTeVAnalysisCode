795f39aa37c69f60da7f4b0b82002f6f0c1fa4a7
Clint <clint.a.richardson@gmail.com>
2017-02-22 15:38:29 -0600

    adding duplicate check script

---
+++ test/dupCheck.cc
@@ -0,0 +1,34 @@
+void dupCheck(){
+
+  set <long long> checkDuplicates;
+  int nDup = 0;
+  long long RUNPREF = 1000 * 1000;
+  RUNPREF *= 1000 * 1000;
+
+  TFile* fd = new TFile("ChargeMisID_MuCBTightMiniIsoTight_ElMVA2016TightRC_2016E-H.root");
+
+  TTree* t = (TTree*) fd->Get("tEvts_ssdl");
+  int run,lumi;
+  long long event;
+  t->SetBranchAddress("Run",&run);
+  t->SetBranchAddress("Lumi",&lumi);
+  t->SetBranchAddress("Event",&event);
+  for(int i=0; i< t->GetEntries();i++){
+    t->GetEntry(i);
+    if(run ==279931 && event==466046051){
+      std::cout<<"run: "<<run<<" lumi: "<<lumi<<" event: "<<event<<std::endl;
+    }
+    long long dupCheck = run * RUNPREF + event;
+    if (checkDuplicates.find(dupCheck) != checkDuplicates.end()){
+      nDup++;
+      std::cout<<"run: "<<run<<" lumi: "<<lumi<<" event: "<<event<<std::endl;
+      continue;
+    }
+    else checkDuplicates.insert(dupCheck);
+  }
+
+
+
+  std::cout<<"number duplicates: "<<nDup<<std::endl;
+
+}
